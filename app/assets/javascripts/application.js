6bffdeed29649b14f8e1d93a57897feb2f405aa8
Pierre Mesure <pierre.mesure@gmail.com>
2019-07-17 10:02:43 +0200

    Add compatibility file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -146,8 +146,6 @@ var initialize_modules = function() {
 };
 
 $(function(){
-  Turbolinks.enableProgressBar();
-
   $(document).ready(initialize_modules);
   $(document).on("turbolinks:load", initialize_modules);
   $(document).on("ajax:complete", initialize_modules);
